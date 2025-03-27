ce25ee3c24732565e94f1841f855d53c6e018d22
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-27 09:05:34 +0100

    Add job struct main file

--- go-playground/main.go
+++ go-playground/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-playground/models"
+)
 
 func main() {
 	fmt.Println("Hello, Go playground")
+	job := models.Job{
+		ID: 1,
+		Company: "antdevrealm",
+		Title: "Go Developer",
+		Status: "Applied",
+	}
+
+	fmt.Println("Job Application:")
+	fmt.Printf("Company: %s\nTitle: %s\nStatus: %s\n", job.Company, job.Title, job.Status)
 }
---
+++ go-playground/models/job.go
@@ -0,0 +1,8 @@
+package models
+
+type Job struct {
+	ID      int
+	Company string
+	Title   string
+	Status  string
+}
