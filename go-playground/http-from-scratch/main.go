382d534916d24aa738a5c20b53b72f22fa672275
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-28 15:55:33 +0100

    Parse HTTP request line from raw TCP data

--- go-playground/http-from-scratch/main.go
+++ go-playground/http-from-scratch/main.go
@@ -5,22 +5,24 @@ import (
 	"io"
 	"log"
 	"net"
+	"strings"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":8080")
+	listen, err := net.Listen("tcp", ":8080")
+
 	if err != nil {
-		log.Fatal("Failed to start server:", err)
+		log.Fatal("Error occurred starting the server: ", err)
 	}
+
 	fmt.Println("Server listening on http://localhost:8080")
 
 	for {
-		conn, err := listener.Accept()
+		conn, err := listen.Accept()
 		if err != nil {
-			log.Println("Failed to accept connection:", err)
+			fmt.Println("Failed to accept connection: ", err)
 			continue
 		}
-
 		go handleConnection(conn)
 	}
 }
@@ -34,23 +36,42 @@ func handleConnection(conn net.Conn) {
 		n, err := conn.Read(buf)
 		if err != nil {
 			if err != io.EOF {
-				log.Println("Error reading from connection:", err)
+				log.Println("Error reading from a file: ", err)
 			}
 			break
 		}
-		if n > 0 {
-			fmt.Println("📥 Received HTTP request:")
-			fmt.Println(string(buf[:n]))
 
-			response := "HTTP/1.1 200 OK\r\n" +
-				"Content-Type: text/plain\r\n" +
-				"Content-Length: 16\r\n" +
-				"\r\n" +
-				"Hello from Go!"
+		rawRequest := string(buf[:n])
+		lines := strings.Split(rawRequest, "\r\n")
+		requestLine := lines[0]
+		parts := strings.Split(requestLine, " ")
+
+		if len(parts) == 3 {
+			method := parts[0]
+			path := parts[1]
+			version := parts[2]
+
+			fmt.Println("Method: " + method)
+			fmt.Println("Path: " + path)
+			fmt.Println("Version: " + version)
+		}
+
+		if n > 0 {
+			fmt.Println("Received HTTP request:")
+			fmt.Println(rawRequest)
+
+			body := "Hello from antdevrealm"
+			response := fmt.Sprintf("HTTP/1.1 200 OK\r\n"+
+				"Content-Type: text/plain\r\n"+
+				"Content-Length: %d\r\n"+
+				"\r\n%s",
+				len(body), body,
+			)
 
 			_, err := conn.Write([]byte(response))
+
 			if err != nil {
-				log.Println("Error writing response:", err)
+				log.Println("Error writing message: ", err)
 			}
 			break
 		}
